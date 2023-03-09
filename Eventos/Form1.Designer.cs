c5ca97d1f4de7bad5bb0d569bc3bcdfd29542619
Saul Perez Lopez <drsaul1984@gmail.com>
2023-03-08 21:45:39 -0800

    Falta un evento del sistema

--- Eventos/Form1.Designer.cs
+++ Eventos/Form1.Designer.cs
@@ -30,10 +30,11 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button1 = new System.Windows.Forms.Button();
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.darkmode = new System.Windows.Forms.RadioButton();
+            this.btnCerrar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,62 +42,85 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(139, 243);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Duplicar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // timer2
             // 
             this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(440, 84);
+            this.pictureBox1.Location = new System.Drawing.Point(22, 94);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(216, 133);
+            this.pictureBox1.Size = new System.Drawing.Size(267, 200);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
+            this.button2.AutoSize = true;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(563, 345);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 46);
+            this.button2.Size = new System.Drawing.Size(140, 46);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Llamar a Label";
+            this.button2.Text = "Mostrar Frases";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // darkmode
+            // 
+            this.darkmode.BackColor = System.Drawing.Color.Transparent;
+            this.darkmode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.darkmode.Location = new System.Drawing.Point(139, 345);
+            this.darkmode.Name = "darkmode";
+            this.darkmode.Size = new System.Drawing.Size(109, 46);
+            this.darkmode.TabIndex = 3;
+            this.darkmode.TabStop = true;
+            this.darkmode.Text = "DarkMode";
+            this.darkmode.UseVisualStyleBackColor = false;
+            this.darkmode.CheckedChanged += new System.EventHandler(this.darkmode_CheckedChanged);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Red;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.Location = new System.Drawing.Point(291, 389);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(108, 36);
+            this.btnCerrar.TabIndex = 4;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.darkmode);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Light Mode";
             this.Shown += new System.EventHandler(this.Form1_Shown_1);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton darkmode;
+        private System.Windows.Forms.Button btnCerrar;
     }
 }
 
--- Eventos/Form1.cs
+++ Eventos/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,22 +13,40 @@ namespace Eventos
 {
     public partial class Form1 : Form
     {
+        public bool lightmode = true;
+        public Label sonder = new Label();
+        public Label sonder2 = new Label();
+        public Label sonder3 = new Label();
+        public Random random = new Random();
+        public Random random2 = new Random();
+        List<Color> colores = new List<Color> { Color.Red, Color.Green, Color.Blue, Color.Yellow };
+        public int indiceColor = 0;
+
         public Form1()
         {
             InitializeComponent();
-            this.Text = " ";
+            this.Text = "LightMode";
             timer1.Interval = 3000;
             timer1.Enabled = false;
             timer2.Interval = 2000;
             timer2.Enabled = false;
+            this.BackgroundImage = Image.FromFile("C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\LightMode.png");            
+            Icon icono = new Icon("C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Bunny.ico");
+            this.Icon = icono;
+            btnCerrar.Hide();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
-        {
-            //this.Visible = false;
-            timer1.Enabled = false;
-            button1.BackColor= Color.Red;
-            
+        {            
+            //timer1.Enabled = false;
+            button2.BackColor = colores[indiceColor];
+            indiceColor++;
+            if (indiceColor >= colores.Count)
+            {
+                indiceColor = 0;
+            }
+
+
         }
 
         private void Form1_Shown_1(object sender, EventArgs e)
@@ -38,35 +57,109 @@ namespace Eventos
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            pictureBox1.Image = Image.FromFile(rutas[contador]);
-
-            contador++;
-
-            if (contador >= rutas.Length)
+            if(lightmode == true)
             {
-                contador = 0;
+                pictureBox1.Image = Image.FromFile(rutas[contador]);
+
+                contador++;
+
+                if (contador >= rutas.Length)
+                {
+                    contador = 0;
+                }
             }
+            else
+            {
+                pictureBox1.Image = Image.FromFile(rutas2[contador]);
+                contador++;
+                if (contador >= rutas2.Length)
+                {
+                    contador = 0;
+                }
+            }
+            
         }
-        private string[] rutas = { "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img1.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img2.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img3.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img4.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img5.png" };
-        private int contador = 0;
+        private string[] rutas = { "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\perrito1.jpg", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\perrito2.jpg", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\perrito3.jpg", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\perrito4.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\perrito5.jpg" };
+        private string[] rutas2 = { "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img1.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img2.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img3.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img4.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img5.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img6.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img7.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img8.png", "C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Img9.png" };
+        private int contador = 0;        
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Random random = new Random();
-            random.Next();
-            Button button2 = new Button();
-            button2.Text = "Nuevo Botón";
-            button2.Location = new Point(50, 50);
-            this.Controls.Add(button2);
-
-        }
-        
         private void button2_Click(object sender, EventArgs e)
         {
-            Label sonder  = new Label();
-            sonder.Text = "Black Metal";
-            sonder.Location = new Point((this.Width/2)-20, (this.Height-20)-30);
+            int[] ejex = Enumerable.Range(0, 500).OrderBy(x => random.Next()).Take(3).ToArray();
+            int[] ejey = Enumerable.Range(0, 300).OrderBy(x => random.Next()).Take(3).ToArray();
+            sonder.AutoSize = true;
+            sonder2.AutoSize = true;
+            sonder3.AutoSize = true;
+            if (lightmode == true)
+            {
+                sonder.Font = new Font("Microsoft Sans Serif", 16, FontStyle.Italic | FontStyle.Bold);
+                sonder.BackColor = Color.Transparent;
+                sonder.Text = "La vida es bella o triste, \nsolo depende de como la queramos ver";
+                sonder.Location = new Point(ejex[0], ejey[0]);
+                this.Controls.Add(sonder);
+                sonder2.Font = new Font("Microsoft Sans Serif", 16, FontStyle.Italic | FontStyle.Bold);
+                sonder2.BackColor = Color.Transparent;
+                sonder2.Text = "No hagas lo que los demás hacen. \nHaz lo que los demás quisieran hacer y no se atreven";
+                sonder2.Location = new Point(ejex[1], ejey[1]);
+                this.Controls.Add(sonder2);
+                sonder3.Font = new Font("Microsoft Sans Serif", 16, FontStyle.Italic | FontStyle.Bold);
+                sonder3.BackColor = Color.Transparent;
+                sonder3.Text = "Si no puedes dejar de pensar en algo,\n no dejes de trabajar en ello";
+                sonder3.Location = new Point(ejex[2], ejey[2]);
+                this.Controls.Add(sonder3);
+            }
+            else
+            {
+                sonder.Font = new Font("Microsoft Sans Serif", 30, FontStyle.Italic | FontStyle.Bold);
+                sonder.BackColor = Color.Transparent;
+                sonder.Text = "666";
+                sonder.ForeColor = Color.Red;
+                sonder.Location = new Point(ejex[0], ejey[0]);
+                this.Controls.Add(sonder);
+                sonder2.Font = new Font("Microsoft Sans Serif", 30, FontStyle.Italic | FontStyle.Bold);
+                sonder2.BackColor = Color.Transparent;
+                sonder2.Text = "SATANAS ES MI PASTOR";
+                sonder.ForeColor = Color.Red;
+                sonder2.Location = new Point(ejex[1], ejey[1]);
+                this.Controls.Add(sonder2);
+                sonder3.Font = new Font("Microsoft Sans Serif", 30, FontStyle.Italic | FontStyle.Bold);
+                sonder3.BackColor = Color.Transparent;
+                sonder3.Text = "696";
+                sonder.ForeColor = Color.Red;
+                sonder3.Location = new Point(ejex[2], ejey[2]);
+                this.Controls.Add(sonder3);
+            }
+            
+        }
+
+        private void darkmode_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            lightmode = false;            
+            //button2.Enabled = lightmode;
+            this.BackgroundImage = Image.FromFile("C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\DarkMode.jpg");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            Icon icono = new Icon("C:\\Users\\Saul\\source\\repos\\Eventos\\Eventos\\Imagenes\\Sauron.ico");
+            this.Icon = icono;
+            sonder.Text = "666";
+            sonder.ForeColor = Color.Red;            
+            sonder2.Text = "SATANAS ES MI PASTOR";
+            sonder2.ForeColor = Color.Red;
+            sonder3.Text = "696";
+            sonder3.ForeColor = Color.Red;
             this.Controls.Add(sonder);
+            this.Controls.Add(sonder2);
+            this.Controls.Add(sonder3);
+            this.FormBorderStyle = FormBorderStyle.None;
+            btnCerrar.Show();
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            int ejex = random.Next(0, this.ClientSize.Width - btnCerrar.Width);
+            int ejey = random.Next(0, this.ClientSize.Height - btnCerrar.Height);
+            btnCerrar.Location = new Point(ejex, ejey);
         }
     }
 }
